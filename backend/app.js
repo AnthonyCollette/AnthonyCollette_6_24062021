e8800fd3b8a1911fb920eef5d0e4f8b7559b1096
Anthony <100pour100inde@gmail.com>
2021-08-13 18:25:39 +0200

    Site done

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,41 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const mongoose = require("mongoose");
-const userRoutes = require("./routes/user");
-const saucesRoutes = require("./routes/sauces");
-const path = require("path");
-const helmet = require("helmet");
-require("dotenv").config();
-
-console.log(process.env.PORT);
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+const saucesRoutes = require('./routes/sauces')
+const path = require('path')
+const helmet = require('helmet')
+require('dotenv').config()
 
 // Permet à tout le monde d'accéder à l'API
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
 
 // Connexion à MondoDB
 
 mongoose
     .connect(process.env.MONGODB_URI, {
-        // dbName: process.env.DB_NAME,
-        // user: process.env.DB_USER,
-        // pass: process.env.DB_PASS,
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(() => console.log("Connexion à MongoDB réussie !"))
-    .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-// mongodb+srv://Admin:admin@piquante.jaoig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Transforme le corps de la requête en objet JavaScript
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(helmet());
+app.use(helmet())
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
-app.use("/api/sauces", saucesRoutes);
-app.use("/api/auth", userRoutes);
+app.use('/api/sauces', saucesRoutes)
+app.use('/api/auth', userRoutes)
 
-module.exports = app;
+module.exports = app
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,20 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const sauceCtrl = require("../controllers/sauce");
-const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
-const checkuserid = require("../middleware/checkuserid");
-// const rateLimit = require("express-rate-limit");
+const express = require('express')
+const router = express.Router()
+const sauceCtrl = require('../controllers/sauce')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+const checkuserid = require('../middleware/checkuserid')
 
-// const limiter = rateLimit({
-//     max: 1,
-// });
+router.post('/', auth, multer, sauceCtrl.createSauce)
+router.delete('/:id', auth, checkuserid, sauceCtrl.deleteSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce)
+router.get('/', auth, sauceCtrl.getAllSauces)
+router.put('/:id', auth, checkuserid, multer, sauceCtrl.modifySauce)
+router.post('/:id/like', auth, sauceCtrl.changeLikes)
 
-router.post("/", auth, multer, sauceCtrl.createSauce);
-router.delete("/:id", auth, checkuserid, sauceCtrl.deleteSauce);
-router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.get("/", auth, sauceCtrl.getAllSauces);
-router.put("/:id", auth, checkuserid, multer, sauceCtrl.modifySauce);
-router.post("/:id/like", auth, sauceCtrl.changeLikes);
-
-module.exports = router;
+module.exports = router
